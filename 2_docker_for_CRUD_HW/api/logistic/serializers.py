cd4d22ceeaa12059e19df2dd637e5ed16fba6b37
Анастасия Золотых <nas60581@yandex.ru>
2023-09-17 19:28:56 +0300

    first commit

---
+++ 2_docker_for_CRUD_HW/api/logistic/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from .models import *
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+           # stock.positions.add(new_position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        for i in positions:
+            StockProduct.objects.update_or_create(defaults={'quantity': i['quantity'], 'price': i['price']},
+                                                  product=i['product'], stock=stock)
+
+        return stock
